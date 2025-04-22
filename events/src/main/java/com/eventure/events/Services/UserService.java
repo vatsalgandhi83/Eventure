4c7d66d41e35a576b9f1c48018ed6e5b714fcf47
harbul <harshita.gupta1502@gmail.com>
2025-04-21 19:16:15 -0700

    fix

--- events/src/main/java/com/eventure/events/Services/UserService.java
+++ events/src/main/java/com/eventure/events/Services/UserService.java
@@ -32,7 +32,7 @@ public class UserService {
 		return userRepo.findById(id);
 	}
 
-/*	// Move this api to AuthService or update the code for authentication
+	// Move this api to AuthService or update the code for authentication
 	public Users addNewUser(Users user) {
 		System.out.println("New User Sign up request =>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>" + user);
 		Optional<Users> existingUser = userRepo.findByEmail(user.getEmail());
